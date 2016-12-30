ca3b6d174a21026074a9c7a123230153bbfb505d
chenhehong <chenhehong@126.com>
2016-12-30 19:20:09 +0800

    修改了知识库的5个图标

--- app/src/main/java/com/scau/easyfarm/fragment/MyFunctionFragment.java
+++ app/src/main/java/com/scau/easyfarm/fragment/MyFunctionFragment.java
@@ -28,27 +28,27 @@ public class MyFunctionFragment extends BaseFunctionFragment{
         Bundle varietyBaseBundle = new Bundle();
         ManualCategory varietyManualCategory = new ManualCategory(0,0,false,"品种库/",ManualCategory.VARIETY,"");
         varietyBaseBundle.putSerializable(BaseManualCategoryListFragment.BUNDLEKEY_PARENT_MANUALCATEGORY, varietyManualCategory);
-        Module varietyBase = new Module(SimpleBackPage.CHOOSE_MANUAL_CATEGORY,"品种库",R.drawable.func_agriculture_knowledge_base_icon,varietyBaseBundle);
+        Module varietyBase = new Module(SimpleBackPage.CHOOSE_MANUAL_CATEGORY,"品种库",R.drawable.func_variety_base_icon,varietyBaseBundle);
         moduleList.add(varietyBase);
         Bundle industryBaseBundle = new Bundle();
         ManualCategory industryManualCategory = new ManualCategory(0,0,false,"产业库/",ManualCategory.INDUSTRY,"");
         industryBaseBundle.putSerializable(BaseManualCategoryListFragment.BUNDLEKEY_PARENT_MANUALCATEGORY, industryManualCategory);
-        Module industryBase = new Module(SimpleBackPage.CHOOSE_MANUAL_CATEGORY,"产业库",R.drawable.func_agriculture_knowledge_base_icon,industryBaseBundle);
+        Module industryBase = new Module(SimpleBackPage.CHOOSE_MANUAL_CATEGORY,"产业库",R.drawable.func_industry_base_icon,industryBaseBundle);
         moduleList.add(industryBase);
         Bundle skillBaseBundle = new Bundle();
         ManualCategory skillManualCategory = new ManualCategory(0,0,false,"技术库/",ManualCategory.TECHNOLOGY,"");
         skillBaseBundle.putSerializable(BaseManualCategoryListFragment.BUNDLEKEY_PARENT_MANUALCATEGORY, skillManualCategory);
-        Module skillBase = new Module(SimpleBackPage.CHOOSE_MANUAL_CATEGORY,"技术库",R.drawable.func_agriculture_knowledge_base_icon,skillBaseBundle);
+        Module skillBase = new Module(SimpleBackPage.CHOOSE_MANUAL_CATEGORY,"技术库",R.drawable.func_skill_base_icon,skillBaseBundle);
         moduleList.add(skillBase);
         Bundle achievementBaseBundle = new Bundle();
         ManualCategory achievementManualCategory = new ManualCategory(0,0,false,"成果库/",ManualCategory.ACHIEVEMENT,"");
         achievementBaseBundle.putSerializable(BaseManualCategoryListFragment.BUNDLEKEY_PARENT_MANUALCATEGORY, achievementManualCategory);
-        Module achievementBase = new Module(SimpleBackPage.CHOOSE_MANUAL_CATEGORY,"成果库",R.drawable.func_agriculture_knowledge_base_icon,achievementBaseBundle);
+        Module achievementBase = new Module(SimpleBackPage.CHOOSE_MANUAL_CATEGORY,"成果库",R.drawable.func_achievement_base_icon,achievementBaseBundle);
         moduleList.add(achievementBase);
         Bundle expertBaseBundle = new Bundle();
         ManualCategory expertManualCategory = new ManualCategory(0,0,false,"专家库/",ManualCategory.EXPERT,"");
         expertBaseBundle.putSerializable(BaseManualCategoryListFragment.BUNDLEKEY_PARENT_MANUALCATEGORY, expertManualCategory);
-        Module expertBase = new Module(SimpleBackPage.EXPERTBASE_CHOOSE_MANUAL_CATEGORY,"专家库",R.drawable.func_agriculture_knowledge_base_icon,expertBaseBundle);
+        Module expertBase = new Module(SimpleBackPage.EXPERTBASE_CHOOSE_MANUAL_CATEGORY,"专家库",R.drawable.func_expert_base_icon,expertBaseBundle);
         moduleList.add(expertBase);
     }
 }
